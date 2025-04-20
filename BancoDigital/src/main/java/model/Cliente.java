8c26f704c385b1b05a51b1ef9e7bc9cb4eaaa172
pablovns <viniciuspablo962@gmail.com>
2025-04-20 20:23:31 -0300

    feat: adiciona @AllArgsConstructor na classe Cliente

--- BancoDigital/src/main/java/model/Cliente.java
+++ BancoDigital/src/main/java/model/Cliente.java
@@ -1,10 +1,12 @@
 package model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
+@AllArgsConstructor
 public class Cliente {
 
 	private String nome;
