851cf485a7e3b743abd9a065e25e53f4b6ed4b46
pablovns <viniciuspablo962@gmail.com>
2025-04-20 21:31:06 -0300

    fix: altera o método main da classe Main para apenas mostrar uma mensagem de boas-vindas.

--- BancoDigital/src/main/java/app/Main.java
+++ BancoDigital/src/main/java/app/Main.java
@@ -1,26 +1,8 @@
 package app;
 
-import exception.*;
-import model.*;
-
 public class Main {
 
 	public static void main(String[] args) {
-		Cliente venilton = new Cliente();
-		venilton.setNome("Venilton");
-		
-		Conta cc = new ContaCorrente(venilton);
-		Conta poupanca = new ContaPoupanca(venilton);
-
-		cc.depositar(100);
-        try {
-            cc.transferir(100, poupanca);
-        } catch (SaldoInsuficienteException e) {
-			System.out.println(e.getMessage());
-        }
-
-        cc.imprimirExtrato();
-		poupanca.imprimirExtrato();
+		System.out.println("Bem vindo ao Banco Digital.");
 	}
-
 }
