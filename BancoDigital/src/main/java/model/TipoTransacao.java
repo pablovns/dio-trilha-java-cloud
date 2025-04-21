9f014d612cf07930d5a9046834905b01582686fd
pablovns <viniciuspablo962@gmail.com>
2025-04-20 21:07:36 -0300

    refac: atualiza taxa de transação para o tipo TRANSFERENCIA
    - altera a taxa da TRANSFERENCIA de 1,0 para 0,01 para refletir melhor a taxa percentual. Também adiciona um comentário de esclarecimento para indicar essa representação.

--- BancoDigital/src/main/java/model/TipoTransacao.java
+++ BancoDigital/src/main/java/model/TipoTransacao.java
@@ -7,13 +7,13 @@ public enum TipoTransacao {
 
     DEPOSITO("Depósito", "D", 0.0, false),
     SAQUE("Saque", "S", 0.0, false),
-    TRANSFERENCIA("Transferência", "T", 1.0, true),
+    TRANSFERENCIA("Transferência", "T", 0.01, true),
     PIX("Pix", "P", 0.0, true)
     ;
 
     private final String descricao;
     private final String codigo;
-    private final double taxa;
+    private final double taxa; // porcentagem da taxa (de 0 a 1)
     private final boolean permiteAgendamento;
 
     TipoTransacao(String descricao, String codigo, double taxa, boolean permiteAgendamento) {
